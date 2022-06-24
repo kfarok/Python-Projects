93f2a6077919b1c1d4116bffa5d4bce707cc589f
kamyab F <64436260+kfarok@users.noreply.github.com>
2022-06-24 17:34:16 +0200

    Update run.py

--- badoo-bot/run.py
+++ badoo-bot/run.py
@@ -1,22 +1,5 @@
-<html>
-<head>
-<title>run.py</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style type="text/css">
-.s0 { color: #cc7832;}
-.s1 { color: #a9b7c6;}
-</style>
-</head>
-<body bgcolor="#2b2b2b">
-<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#606060" >
-<tr><td><center>
-<font face="Arial, Helvetica" color="#000000">
-run.py</font>
-</center></td></tr></table>
-<pre><span class="s0">from </span><span class="s1">badoo </span><span class="s0">import </span><span class="s1">Bado</span>
+from badoo import Bado
 
-<span class="s0">with </span><span class="s1">Bado() </span><span class="s0">as </span><span class="s1">bot:</span>
-    <span class="s1">bot.login()</span>
-    <span class="s1">bot.auto_like()</span></pre>
-</body>
-</html>
+with Bado() as bot:
+    bot.login()
+    bot.auto_like()
